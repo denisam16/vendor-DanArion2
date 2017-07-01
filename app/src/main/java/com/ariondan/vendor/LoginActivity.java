3f496cad528acfe01fee74f404ccc1c965fd9885
Akitektuo <alexcopindean@yahoo.com>
2017-07-01 17:18:16 +0300

    Finished log in activity example

--- app/src/main/java/com/ariondan/vendor/LoginActivity.java
+++ app/src/main/java/com/ariondan/vendor/LoginActivity.java
@@ -1,40 +1,93 @@
 package com.ariondan.vendor;
 
-import android.content.Intent;
-import android.support.annotation.IdRes;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Override
-    public void onClick(View view) {
-    }
-    private EditText editUsername;
-    private EditText editPassword;
-    @Override
+    private MaterialEditText editEmail;
+    private MaterialEditText editPassword;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        editUsername = (EditText) findViewById(R.id.edit_login_username);
-        editPassword = (EditText) findViewById(R.id.edit_login_password);
+        editEmail = (MaterialEditText) findViewById(R.id.edit_login_email);
+        editPassword = (MaterialEditText) findViewById(R.id.edit_login_password);
+        findViewById(R.id.button_login).setOnClickListener(this);
+        findViewById(R.id.text_forget_password).setOnClickListener(this);
+    }
 
-        Button buttonLogin = (Button) findViewById(R.id.button_login);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_login:
+                logIn();
+                break;
+            case R.id.text_forget_password:
+                requestForgottenPassword();
+                break;
+        }
+    }
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+    private void logIn() {
+        String email = editEmail.getText().toString(), password = editPassword.getText().toString();
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Fill in all fields.", Toast.LENGTH_SHORT).show();
+        } else {
+            if (email.contains("@")) {
+                if (email.equals("adimn@gmail.com") && password.equals("parola")) {
+                    //go to store
+                } else {
+                    Toast.makeText(this, "Wrong e-mail or password.", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Invalid e-mail.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void requestForgottenPassword() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View viewDialog = LayoutInflater.from(this).inflate(R.layout.dialog_forgotten_password, null);
+        builder.setView(viewDialog);
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view){
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                finish();
+            public void onClick(DialogInterface dialog, int which) {
+                String email = ((MaterialEditText) viewDialog.findViewById(R.id.edit_dialog_email)).getText().toString();
+                if (email.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Field was empty.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (email.contains("@")) {
+                        if (email.equals("admin@gmail.com")) {
+                            Toast.makeText(LoginActivity.this, "E-mail has been sent.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Wrong e-mail.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid e-mail.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
-
-
-
+        builder.setNegativeButton("Cancel", null);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+        alertDialog.show();
     }
+
 }
